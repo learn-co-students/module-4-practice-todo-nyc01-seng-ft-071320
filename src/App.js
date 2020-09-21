8b4d4c48a45e91592532751c9354f3151717ebe3
humorous-storage-7521 <rmwerner@umich.edu>
2020-09-21 10:00:27 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { CATEGORIES } from './data'
+import TaskContainer from './containers/TaskContainer'
+import ButtonContainer from './containers/ButtonContainer'
 
 class App extends React.Component {
 
@@ -34,13 +36,41 @@ class App extends React.Component {
         text: 'Tidy house',
         category: 'Misc'
       }
-    ]
+    ],
+    selected: 'All'
+  }
+
+  buttonClickHandler = (cat) => {
+    this.setState({selected: cat})
+  }
+
+  taskClickHandler = (deletedTask) => {
+    let newArray = this.state.tasks
+    let index = newArray.findIndex(task => task === deletedTask)
+    newArray.splice(index, 1)
+    this.setState({tasks: newArray})
+  }
+
+  filterTasks = () => {
+    if (this.state.selected === 'All') {
+      return this.state.tasks
+    } else {
+      return this.state.tasks.filter(task => task.category === this.state.selected)
+    }
+  }
+
+  submitHandler = (task) => {
+    let newArray = this.state.tasks
+    newArray.push(task)
+    this.setState({tasks: newArray})
   }
 
   render() {
     return (
       <div className="App">
         <h2>My tasks</h2>
+        <ButtonContainer categories={CATEGORIES} clickHandler={this.buttonClickHandler} selected={this.state.selected} />
+        <TaskContainer tasks={this.filterTasks()} categories={CATEGORIES} clickHandler={this.taskClickHandler} submitHandler={this.submitHandler}/>
       </div>
     );
   }
